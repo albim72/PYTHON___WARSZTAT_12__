ddf8f888769957668d258fa52188692a04469cc2
albim72 <marcin.albiniak72@gmail.com>
2022-12-28 13:23:43 +0100

    Update lambda.py

--- DZIEN_1/lambda.py
+++ DZIEN_1/lambda.py
@@ -16,3 +16,7 @@ def multi(n):
     return lambda a:a*n*math.sqrt(a)
 
 print(multi(6)(7))
+num = [67,-2,5,2,177,-9,34,122,4,7,1,100,-4,0,5,16,78,93,-56,12]
+
+#stwórz listę nieparz i przekaż do niej wszystkie wartości nieparzyste z listy num
+#użyj funkcji standardowej filter(funkcja,dane) - pierwszy parametr musi być funkcją opisującą warunek filtrowania
