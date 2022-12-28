c6bb755884c3c71929e6d5e16d1ca0b6e3ec1bed
albim72 <marcin.albiniak72@gmail.com>
2022-12-28 13:07:09 +0100

    Update rozne_funkcje.py

--- DZIEN_1/rozne_funkcje.py
+++ DZIEN_1/rozne_funkcje.py
@@ -25,3 +25,10 @@ print(gx(5,8))
 print(gx(9))
 print(gx(11,7,b=15))
 
+#przykład 3
+def ranking(*lang,nrrank):
+    print(f'ranking języków programowania: pierwsze miejsce -> {lang[0]}, drugie miejsce -> '
+          f'{lang[1]}, trzecie miejsce: {lang[2]} - nr rankingu: {nrrank}, liczba jęzków w zestawieniu: {len(lang)}')
+
+ranking("Java","C++","Python",nrrank=67)
+ranking("Python","Java","C++","C#","Perl","Object Pascal",nrrank=69)
