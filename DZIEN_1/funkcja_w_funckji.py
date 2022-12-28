d42e2bd63bb9a9c3a2cf49ac59c6ede12ae713dc
albim72 <marcin.albiniak72@gmail.com>
2022-12-28 13:46:31 +0100

    Update funkcja_w_funckji.py

--- DZIEN_1/funkcja_w_funckji.py
+++ DZIEN_1/funkcja_w_funckji.py
@@ -3,7 +3,11 @@
 def witaj(imie):
     return f'Miło Cię widzieć {imie}'
 
-def osoba(funkcja,imie):
-    return funkcja(imie)
+def konkurs(imie,punkty):
+    return f'uczetnik konkursu {imie}, liczba punktów: {punkty}'
+
+def osoba(funkcja,*args):
+    return funkcja(*args)
 
 print(osoba(witaj,"Leon"))
+print(osoba(konkurs,"Olga",56))
