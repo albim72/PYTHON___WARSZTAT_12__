88566500198c8029a12959697db2cc7f6b546d45
albim72 <marcin.albiniak72@gmail.com>
2022-12-28 13:28:10 +0100

    Update lambda.py

--- DZIEN_1/lambda.py
+++ DZIEN_1/lambda.py
@@ -20,3 +20,15 @@ num = [67,-2,5,2,177,-9,34,122,4,7,1,100,-4,0,5,16,78,93,-56,12]
 
 #stwórz listę nieparz i przekaż do niej wszystkie wartości nieparzyste z listy num
 #użyj funkcji standardowej filter(funkcja,dane) - pierwszy parametr musi być funkcją opisującą warunek filtrowania
+
+nieparz = list(filter(lambda x:x%2!=0, num))
+print(nieparz)
+
+def warunek(x):
+    return x%2!=0
+
+nparz_f = list(filter(warunek,num))
+print(nparz_f)
+
+#stwórz listę cube i przekaż do niej wszystkie wartości z listy num podniesione do potęgi 3
+#użyj funkcji standardowej map(funkcja,dane) - pierwszy parametr musi być funkcją opisującą warunek transformacji
