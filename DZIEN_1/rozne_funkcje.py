a1dab025cd6c645d1d4752ae4804fec67947a2fe
albim72 <marcin.albiniak72@gmail.com>
2022-12-28 13:00:18 +0100

    Update rozne_funkcje.py

--- DZIEN_1/rozne_funkcje.py
+++ DZIEN_1/rozne_funkcje.py
@@ -1,4 +1,6 @@
 #różne definicje funkcji
+#przykład 1
+import math
 
 def fx(h):
     return h**4
@@ -11,3 +13,15 @@ def policz(a,b,c,y):
 
 print(policz(3,6,7,3))
 print(n)
+
+#przykład 2
+
+def gx(n,m=3,k=3,b=5):
+    return math.sqrt(n+m)*k-b
+
+print(gx(2,7,8,12))
+print(gx(6,4,2))
+print(gx(5,8))
+print(gx(9))
+print(gx(11,7,b=15))
+
