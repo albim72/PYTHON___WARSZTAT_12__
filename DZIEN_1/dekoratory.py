5dc923afc07605a3cc185cb3eeafd5491cf59be2
albim72 <marcin.albiniak72@gmail.com>
2022-12-28 14:44:23 +0100

    Update dekoratory.py

--- DZIEN_1/dekoratory.py
+++ DZIEN_1/dekoratory.py
@@ -9,7 +9,15 @@ def pomiarczasu(funkcja):
         print(f'czas wykonania funkcji: {endtime-starttime} s')
     return wrapper
 
+
+def sleep(funkcja):
+    def wrapper():
+        time.sleep(3)
+        return funkcja()
+    return wrapper
+
 @pomiarczasu
+@sleep
 def big_lista():
     sum([i**5 for i in range(1000000)])
 
@@ -21,3 +29,11 @@ def policz(a,b,c):
     return (a+b)-c
 
 policz(6,3,6)
+
+lt = [i**5 for i in range(1000000)]
+@pomiarczasu
+def gotowa_lista():
+    sum(lt)
+
+print("________________________________________")
+gotowa_lista()
