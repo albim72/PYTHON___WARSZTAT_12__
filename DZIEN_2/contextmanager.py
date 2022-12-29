510b71738edfc1be3eef71e7419a153d5e077bc5
albim72 <marcin.albiniak72@gmail.com>
2022-12-29 14:48:55 +0100

    Create contextmanager.py

---
+++ DZIEN_2/contextmanager.py
@@ -0,0 +1,14 @@
+class ContextManager(object):
+    def __init__(self):
+        print("inicjacja metody __init__")
+
+    def __enter__(self):
+        print("inicjacja metody __enter__")
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print("inicjacja metody __exit__")
+
+
+with ContextManager() as manager:
+    print("to jest ciało instrukcji with")
