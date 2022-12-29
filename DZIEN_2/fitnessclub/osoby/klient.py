169c4bebfd00191857f3c436d01b06cecee2ec4b
albim72 <marcin.albiniak72@gmail.com>
2022-12-29 11:52:11 +0100

    Update klient.py

--- DZIEN_2/fitnessclub/osoby/klient.py
+++ DZIEN_2/fitnessclub/osoby/klient.py
@@ -2,9 +2,9 @@ from osoby.trener import Trener
 from rep.sport import Sport
 
 class Klient(Trener,Sport):
-    def __init__(self,nr_klienta,imie,wiek,waga,wzrost,nr_kb,nazwa,miasto,dyscyplina,lata_upr,zyciowka,nr_licencji=""):
-        Trener.__init__(self,imie,wiek,waga,wzrost,
-                 nr_kb,nazwa,miasto,nr_licencji,lata_dosw="",expert="")
+    def __init__(self,nr_klienta,imie,wiek,waga,wzrost,nr_kb,nazwa,miasto,dyscyplina,lata_upr,zyciowka,
+                 nr_licencji=""):
+        Trener.__init__(self,imie,wiek,waga,wzrost,nr_licencji,"","",nr_kb,nazwa,miasto)
         Sport.__init__(self,dyscyplina,lata_upr,zyciowka)
         self.nr_klienta = nr_klienta
         self.numerklienta()
@@ -12,5 +12,3 @@ class Klient(Trener,Sport):
 
     def numerklienta(self):
         print(f'przydzielono nr klienta: {self.nr_klienta}')
-
-        
