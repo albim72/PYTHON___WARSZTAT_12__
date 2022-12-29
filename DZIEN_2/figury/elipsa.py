f2736edf6ff30a5d450386d4c7589e24ec08769d
albim72 <marcin.albiniak72@gmail.com>
2022-12-29 13:47:11 +0100

    Update elipsa.py

--- DZIEN_2/figury/elipsa.py
+++ DZIEN_2/figury/elipsa.py
@@ -1,7 +1,13 @@
 from figura import Figura
+from kolo import Kolo
 import math
 
 class Elipsa(Figura):
+
+    def __new__(cls, a:float, b:float):
+        if a==b:
+            return Kolo(a=a)
+        return object.__new__(Elipsa)
 
     def pole_figury(self):
         return math.pi*self.a*self.b
