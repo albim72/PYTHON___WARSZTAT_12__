c303f34b765f238dec47723453264819b4dae3e8
albim72 <marcin.albiniak72@gmail.com>
2022-12-29 10:01:01 +0100

    Update main.py

--- DZIEN_2/mojemoduly/main.py
+++ DZIEN_2/mojemoduly/main.py
@@ -2,6 +2,16 @@
 #import dane as dn
 from dane import filia_nb as filia
 from dane import book as ksiazka
+from funkcje.bfunkcje import czytaj, czytaj_slownik
+from klasy.KlasaDanych import KlasaDanych
 
 print(filia)
 print(ksiazka)
+
+print("___________________czytanie za pomocą funkcji_______________________")
+czytaj(filia)
+czytaj_slownik(ksiazka)
+print("___________________czytanie za pomocą metod klasy_______________________")
+kd = KlasaDanych(filia,ksiazka)
+kd.rlista()
+kd.rslownik()
