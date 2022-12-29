34b9778a18f72d82b5d1acad502f811aa79b9a2e
albim72 <marcin.albiniak72@gmail.com>
2022-12-29 13:18:16 +0100

    Update prostokat.py

--- DZIEN_2/figury/prostokat.py
+++ DZIEN_2/figury/prostokat.py
@@ -1,6 +1,12 @@
 from figura import Figura
+from kwadrat import Kwadrat
 
 class Prostokat(Figura):
+
+    def __new__(cls, a:float, b:float):
+        if a==b:
+            return Kwadrat(a=a)
+        return object.__new__(Prostokat)
 
     def pole_figury(self):
         return self.a*self.b
