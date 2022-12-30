bf86b8eb2db631133549ceb9901672a8977f0fbd
albim72 <marcin.albiniak72@gmail.com>
2022-12-30 13:26:52 +0100

    Update xmlsourse.py

--- DZIEN_3/xmlsourse.py
+++ DZIEN_3/xmlsourse.py
@@ -7,5 +7,8 @@ try:
     for child in root:
         print(f'element: {child.tag}, atrybuty: {child.attrib}')
 
+    print(root[0])
+    print(root[0][2])
+    print(root[0][2].text)
 except:
     print("element nie istnieje")
