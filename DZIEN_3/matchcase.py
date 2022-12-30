246450dc1b414c0fedc221bc774c264f4db6365d
albim72 <marcin.albiniak72@gmail.com>
2022-12-30 09:10:13 +0100

    Create matchcase.py

---
+++ DZIEN_3/matchcase.py
@@ -0,0 +1,18 @@
+dzien  = 15
+match dzien:
+    case 1:
+        print("poniedziałek")
+    case 2:
+        print("wtorek")
+    case 3:
+        print("środa")
+    case 4:
+        print("czwartek")
+    case 5:
+        print("piątek")
+    case 6:
+        print("sobota")
+    case 7:
+        print("niedziela")
+    case _:
+        print("zły wybór")
