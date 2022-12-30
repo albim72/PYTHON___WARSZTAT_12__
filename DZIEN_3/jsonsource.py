bd0f5f075ecfce89ac95806a17b4a5166acc3002
albim72 <marcin.albiniak72@gmail.com>
2022-12-30 12:09:43 +0100

    Update jsonsource.py

--- DZIEN_3/jsonsource.py
+++ DZIEN_3/jsonsource.py
@@ -17,3 +17,17 @@ kolor = {
     "lodcieni":3,
     "odcienie":["jasny","standard","ciemny"]
 }
+
+jsonkolor = json.dumps(kolor,indent=4)
+print(jsonkolor)
+
+with open("kolor.json","w",encoding="utf-8") as f:
+    f.write(jsonkolor)
+
+with open("kolor.json","r",encoding="utf-8") as f:
+    kolor_dict = json.load(f)
+
+print(kolor_dict)
+print(kolor_dict['odcienie'][0])
+
+print("____________________________________________")
