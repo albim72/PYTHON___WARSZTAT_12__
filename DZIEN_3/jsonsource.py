976c87311e98f8791700b6a788fa4614bf21e418
albim72 <marcin.albiniak72@gmail.com>
2022-12-30 12:05:13 +0100

    Update jsonsource.py

--- DZIEN_3/jsonsource.py
+++ DZIEN_3/jsonsource.py
@@ -1,3 +1,19 @@
 import json
 
 jsonbook = '{"tytul":"Hobbit","autor":"J.R.R. Tolkien","oprawa":"twarda","lstron":245,"cena":23.77}'
+print(jsonbook)
+print(type(jsonbook))
+
+book = json.loads(jsonbook)
+print(book)
+print(type(book))
+
+print(book['autor'])
+
+kolor = {
+    "id":23423,
+    "nazwa_koloru":"zółty ciemny",
+    "paleta":"UC567",
+    "lodcieni":3,
+    "odcienie":["jasny","standard","ciemny"]
+}
