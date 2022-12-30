1b866eb29661227e7cd6f312a3e9e306c1f69305
albim72 <marcin.albiniak72@gmail.com>
2022-12-30 13:40:57 +0100

    Update main.py

--- DZIEN_3/MINIDOM/main.py
+++ DZIEN_3/MINIDOM/main.py
@@ -4,8 +4,12 @@ dom = parse("dane.xml")
 name = dom.getElementsByTagName('name')
 kod = dom.getElementsByTagName('kod')
 url = dom.getElementsByTagName('url')
+opis = dom.getElementsByTagName('opis')
 
 
 print(" ".join(t.nodeValue for t in name[0].childNodes if t.nodeType==t.TEXT_NODE))
 print(" ".join(t.nodeValue for t in kod[0].childNodes if t.nodeType==t.TEXT_NODE))
+print(" ".join(t.nodeValue for t in kod[1].childNodes if t.nodeType==t.TEXT_NODE))
 print(" ".join(t.nodeValue for t in url[0].childNodes if t.nodeType==t.TEXT_NODE))
+print(" ".join(t.nodeValue for t in url[1].childNodes if t.nodeType==t.TEXT_NODE))
+print(" ".join(t.nodeValue for t in opis[0].childNodes if t.nodeType==t.TEXT_NODE))
